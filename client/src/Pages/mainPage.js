a8a7fdcbe0260591b2b9e20cd482d639cc203760
채윤선 <yoonsun0809@gmail.com>
2023-04-28 01:57:35 +0900

    feat 뉴스 크롤링

--- client/src/Pages/mainPage.js
+++ client/src/Pages/mainPage.js
@@ -4,9 +4,19 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { Desktop } from "../Components/Desktop";
 import Header from "../Components/Header";
+import axios from "axios";
 
 function MainPage() {
   const navigate = useNavigate();
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:5000/api/news");
+      setArticles(response.data);
+    };
+    fetchData();
+  }, []);
 
   const onClickLogout = () => {
     sessionStorage.clear();
@@ -17,6 +27,15 @@ function MainPage() {
     <div>
       <Desktop>
         <Header logout={onClickLogout} logoWidth={"100px"} />
+        <ul>
+          {articles.map((article) => (
+            <li key={article.url}>
+              <img src={article.imageUrl} alt={article.title} />
+              <a href={article.url}>{article.title}</a>
+              <p>{article.author}</p>
+            </li>
+          ))}
+        </ul>
       </Desktop>
     </div>
   );
--- server.js
+++ server.js
@@ -2,11 +2,43 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello Express!" });
+const axios = require("axios");
+const cheerio = require("cheerio");
+const iconv = require("iconv-lite");
+
+app.get("/api/news", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=105&sid2=731",
+      { responseType: "arraybuffer", responseEncoding: "binary" }
+    );
+    const content = iconv.decode(response.data, "EUC-KR").toString();
+    const $ = cheerio.load(content);
+    const newsList = [];
+
+    $("div.list_body > ul > li").each((index, element) => {
+      const author = $(element).find("span.writing").text();
+      const title = $(element)
+        .find("a")
+        .text()
+        .replace(/\n|\r|\s*/g, "")
+        .trim();
+      const url = $(element).find("a").attr("href");
+      const imageUrl = $(element).find("img").attr("src");
+
+      newsList.push({ author, title, url, imageUrl });
+    });
+
+    res.json(newsList);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
