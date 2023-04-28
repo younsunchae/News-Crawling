cdfcadddee645179af488e6eaa2041898dd42476
채윤선 <yoonsun0809@gmail.com>
2023-04-29 06:31:12 +0900

    style 스타일 수정

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
-import TableFooter from "@mui/material/TableFooter";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import { Box, Table, TableBody, Typography } from "@mui/material";
+import TableFooter from "@mui/material/TableFooter";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 
-const Footer = (props) => {
-  const { articles } = props;
-
+const Footer = ({ articles }) => {
   const authorCounts = {};
   for (let article of articles) {
     if (authorCounts[article.author]) {
@@ -22,30 +26,22 @@ const Footer = (props) => {
         display: "flex",
         flexDirection: "column",
         alignItems: "flex-start",
+        position: "sticky",
+        bottom: 0,
       }}
     >
       <Typography
         sx={{
           fontWeight: "bold",
-          display: "inline-block",
-          whiteSpace: "nowrap",
           marginLeft: "15px",
         }}
       >
         작성자: 기사수
       </Typography>
-      <Table
-        border="none"
-        sx={{
-          flexDirection: "column",
-          display: "inline-block",
-          border: "none",
-          paddingTop: "3px",
-        }}
-      >
+      <Table>
         <TableBody>
           <TableRow sx={{}}>
-            <TableCell sx={{ border: "none", fontSize: "15px" }}>
+            <TableCell sx={{ border: "none", fontSize: "15px" }} align="left">
               {Object.keys(authorCounts).map((author) => (
                 <span key={author}>
                   {author}: {authorCounts[author]} | &nbsp;
